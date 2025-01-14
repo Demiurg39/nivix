3ebda472bd6dde4e6adcdf2fe80b0e53067894f2
Demiurg39 <chiryagov2014@gmail.com>
2025-01-14 16:05:18 +0300

    added nix ls

--- config/langs/nix.nix
+++ config/langs/nix.nix
@@ -1,3 +1,14 @@
-{ ... }: {
+{ mkKey, lib, pkgs, ... }: {
+
+  plugins = {
+    lsp = {
+      enable = lib.mkDefault true;
+      inlayHints = lib.mkDefault true;
+      servers.nil_ls = {
+        enable = true;
+        settings.formatting.command = [ "${lib.getExe pkgs.nixpkgs-fmt}" ];
+      };
+    };
+  };
 
 }
