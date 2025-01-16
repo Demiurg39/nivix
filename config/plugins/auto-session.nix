a12d5e61ca56912f9d229b3dd480cf35fe3838d5
Demiurg39 <chiryagov2014@gmail.com>
2025-01-16 16:02:40 +0300

    auto-session config added

--- config/plugins/auto-session.nix
+++ config/plugins/auto-session.nix
@@ -1,3 +1,21 @@
-{ ... }: {
+{ mkKey, ... }: let
+  inherit (mkKey) mkKeymap;
+in {
+
+  plugins.auto-session = {
+    enable = true;
+    settings = {
+      auto-save = true;
+      auto-restore = false;
+    };
+  };
+
+  keymaps = [
+    (mkKeymap "n" "<leader>q." "<cmd>SessionRestore<CR>" "Last Session")
+    (mkKeymap "n" "<leader>ql" "<cmd>Autosession search<CR>" "List Session")
+    (mkKeymap "n" "<leader>qd" "<cmd>Autosession delete<CR>" "Delete Session")
+    (mkKeymap "n" "<leader>qs" "<cmd>SessionSave<CR>" "Save Session")
+    (mkKeymap "n" "<leader>qD" "<cmd>SessionPurgeOrphaned<CR>" "Purge Orphaned Sessions")
+  ];
 
 }
