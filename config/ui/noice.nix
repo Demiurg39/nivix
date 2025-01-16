45b3d5231ff7c98ff0d6eb9ee15cc6ca6873442b
Demiurg39 <chiryagov2014@gmail.com>
2025-01-16 14:55:18 +0300

    noice config added

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -1,3 +1,20 @@
 { ... }: {
 
+  plugins.noice = {
+    enable = true;
+    settings = {
+      lsp = {
+        override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+          "cmp.entry.get_documentation" = true;
+        };
+        hover.enabled = false;
+        messages.enabled = false;
+        signature.enabled = false;
+        progress.enabled = false;
+      };
+    };
+  };
+
 }
