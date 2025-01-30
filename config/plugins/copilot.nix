dfd09c6691ed4d4f7c9474c83cdaea49964f7da0
Demiurg39 <chiryagov2014@gmail.com>
2025-01-30 10:50:32 +0300

    copilot config added

--- config/plugins/copilot.nix
+++ config/plugins/copilot.nix
@@ -1,3 +1,53 @@
-{ ... }: {
+{ mkKey
+, specObj
+, helpers
+, ...
+}:
+let
+  inherit (mkKey) mkKeymap;
+in
+{
+  plugins.copilot-lua = {
+    enable = true;
+    settings = {
+      filetypes = {
+        markdown = true;
+        "*" = false;
+      };
+      suggestion = {
+        enabled = true;
+        autoTrigger = true;
+        keymap = {
+          accept = "<C-cr>";
+          accept_word = false;
+          accept_line = false;
+          next = "<C-]>";
+          prev = "<C-[>";
+          dismiss = "<C-bs>";
+        };
+      };
+    };
+  };
 
+  wKeyList = [ (specObj [ "<leader>a" "󰚩" "ai" ]) ];
+
+  keymaps = [
+    (mkKeymap "n" "<leader>ac"
+      (helpers.mkRaw # lua
+        ''
+          function()
+            if vim.g.copilot_status == nil then
+              vim.g.copilot_status = "running"
+            end
+            if vim.g.copilot_status == "running" then
+              vim.g.copilot_status = "stopped"
+              vim.cmd("Copilot disable")
+            else
+              vim.g.copilot_status = "running"
+              vim.cmd("Copilot enable")
+            end
+          end
+        '')
+      "Toggle Copilot")
+  ];
 }
