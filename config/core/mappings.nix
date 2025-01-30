7252540d1e643dacaebad78ed2d148da8fe9fb59
Demiurg39 <chiryagov2014@gmail.com>
2025-01-30 11:10:34 +0300

    <A-q> mapping added for bdelete

--- config/core/mappings.nix
+++ config/core/mappings.nix
@@ -1,12 +1,13 @@
-{ mkKey, specObj, ... }: let
+{ mkKey, specObj, helpers, ... }:
+let
   inherit (mkKey) mkKeymap;
 
   insert = [
     (mkKeymap "i" "<C-b>" "<Esc>^i" "Move: beginning of line")
-    (mkKeymap "i" "<C-e>" "<End>"   "Move: end of line")
-    (mkKeymap "i" "<C-h>" "<Left>"  "Move: Left")
-    (mkKeymap "i" "<C-j>" "<Down>"  "Move: Down")
-    (mkKeymap "i" "<C-k>" "<Up>"    "Move: Up")
+    (mkKeymap "i" "<C-e>" "<End>" "Move: end of line")
+    (mkKeymap "i" "<C-h>" "<Left>" "Move: Left")
+    (mkKeymap "i" "<C-j>" "<Down>" "Move: Down")
+    (mkKeymap "i" "<C-k>" "<Up>" "Move: Up")
     (mkKeymap "i" "<C-l>" "<Right>" "Move: Right")
   ];
   normal = [
@@ -14,15 +15,41 @@
 
     (mkKeymap "n" "<A-i>" "<Esc>:bnext<cr>" "Buffer: Next")
     (mkKeymap "n" "<A-m>" "<Esc>:bprev<cr>" "Buffer: Previous")
+    (mkKeymap "n" "<A-q>"
+      (helpers.mkRaw # lua
+        ''
+          function()
+            local wins = vim.api.nvim_tabpage_list_wins(0)
+            local non_floating_wins = 0
 
-    (mkKeymap "n" "<leader><tab>i" "<cmd>tabn<cr>"     "Tab: Next Tab")
-    (mkKeymap "n" "<leader><tab>m" "<cmd>tabp<cr>"     "Tab: Previous Tab")
+            -- Count non-floating windows
+            for _, win in ipairs(wins) do
+              local config = vim.api.nvim_win_get_config(win)
+              if not config.relative or config.relative == "" then
+                non_floating_wins = non_floating_wins + 1
+              end
+            end
+
+            -- Close window or buffer based on the number of non-floating windows
+            if non_floating_wins > 1 then
+              vim.cmd("close")
+            elseif #vim.fn.getbufinfo({buflisted = 1}) > 1 then
+              vim.cmd('bdelete')  -- Delete the buffer if more than one buffer is open
+            else
+              vim.cmd('quit')  -- Quit Neovim if it's the last buffer
+            end
+          end
+        '')
+      "Buffer Close")
+
+    (mkKeymap "n" "<leader><tab>i" "<cmd>tabn<cr>" "Tab: Next Tab")
+    (mkKeymap "n" "<leader><tab>m" "<cmd>tabp<cr>" "Tab: Previous Tab")
     (mkKeymap "n" "<leader><tab>d" "<cmd>tabclose<cr>" "Tab: close Tab")
-    (mkKeymap "n" "<leader><tab>n" "<cmd>tabnew<cr>"   "Tab: new Tab")
+    (mkKeymap "n" "<leader><tab>n" "<cmd>tabnew<cr>" "Tab: new Tab")
 
     (mkKeymap "n" "<leader>sv" "<cmd>vsplit<cr>" "Window: split vertically")
-    (mkKeymap "n" "<leader>sh" "<cmd>split<cr>"  "Window: split horizontally")
-    (mkKeymap "n" "<leader>sd" "<cmd>close<cr>"  "Window: close split")
+    (mkKeymap "n" "<leader>sh" "<cmd>split<cr>" "Window: split horizontally")
+    (mkKeymap "n" "<leader>sd" "<cmd>close<cr>" "Window: close split")
 
   ];
   visual = [
@@ -33,7 +60,8 @@
     (mkKeymap "v" "<A-k>" ":m '<-2<cr>gv=gv" "Move: line Up")
   ];
 
-in {
+in
+{
 
   keymaps = insert ++ normal ++ visual;
   wKeyList = [
