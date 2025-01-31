c075620e6a10428c7e189bd572786d85da57935e
Demiurg39 <chiryagov2014@gmail.com>
2025-01-31 21:13:12 +0300

    lib refactored

--- lib/default.nix
+++ lib/default.nix
@@ -1,25 +1,24 @@
 { lib, pkgs, ... }: {
 
-  mkKey = rec {
-    mkKeymap = mode: key: action: desc: {
-      inherit key;
-      inherit mode;
-      inherit action;
-      options = {
-        inherit desc;
-        silent = true;
-        noremap = true;
-        remap = true;
-      };
+  mkKeymap = mode: key: action: desc: {
+    inherit key;
+    inherit mode;
+    inherit action;
+    options = {
+      inherit desc;
+      silent = true;
+      noremap = true;
+      remap = true;
     };
-
-    mkKeymap' = key: mode: action: (mkKeymap key mode action null);
   };
 
   mkPkgs = name: src: pkgs.vimUtils.buildVimPlugin { inherit name src; };
 
+  readDir = builtins.map (fn: ./${fn}) (builtins.filter (fn: fn != "default.nix") (builtins.attrNames (builtins.readDir ./.)));
+
   specObj = # with builtins;
-    list: let
+    list:
+    let
       len = builtins.length list;
       first = lib.optionalAttrs (builtins.elemAt list 0 != "") { __unkeyed = builtins.elemAt list 0; };
       second = lib.optionalAttrs (builtins.elemAt list 1 != "") { icon = builtins.elemAt list 1; };
