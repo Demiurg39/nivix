eb197bd410b1b9b94cfae5ffd51e31b2a545725a
Demiurg39 <chiryagov2014@gmail.com>
2025-02-01 14:33:04 +0300

    separate diagnostics to separate module

---
+++ config/core/diagnostics.nix
@@ -0,0 +1,39 @@
+{
+  icons,
+  lib,
+  ...
+}: {
+  options = {
+    diagnostics = {
+
+    };
+  };
+
+  config = {
+  extraConfigLua = ''
+    local signs = {
+      Hint = "${icons.diagnostics.BoldHint}",
+      Info = "${icons.diagnostics.BoldInformation}",
+      Warn = "${icons.diagnostics.BoldWarning}",
+      Error = "${icons.diagnostics.BoldError}",
+    }
+
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define( hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    vim.diagnostic.config({
+      virtual_text = false,
+      underline = true,
+      signs = true,
+      severity_sort = true,
+      float = {
+        border = "round",
+        source = "always",
+        focusable = false,
+      }
+    })
+  '';
+  };
+}
