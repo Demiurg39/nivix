a1d764a161f4f2ecf7990ae0e25c79e33016f496
Demiurg39 <chiryagov2014@gmail.com>
2025-02-06 10:32:22 +0300

    java lsp config added

--- config/langs/java.nix
+++ config/langs/java.nix
@@ -9,5 +9,16 @@ in {
     enable = lib.mkEnableOption "Enable java language module";
   };
 
-  config = lib.mkIf cfg.enable {};
+  config = lib.mkIf cfg.enable {
+    plugins.nvim-jdtls = {
+      enable = true;
+      data = "~/.cache/jdtls/workspace";
+      settings = {
+        java = {
+          signatureHelp = true;
+          completion = true;
+        };
+      };
+    };
+  };
 }
